a09addf14bb0be1321bbe6d4c43333e3efe4abb8
Frederik Feldmeier <74424903+elfeldos@users.noreply.github.com>
2024-08-31 15:31:23 +0200

    connected DHT sensor data to LCD display

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,10 @@ LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 void setup( )
 {
   Serial.begin( 9600);
-  // set up the LCD's number of columns and rows:
+  // // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("Moin Franco!");
+  // // Print a message to the LCD.
+  // lcd.print("Moin Franco!");
 }
 
 /*
@@ -36,7 +36,6 @@ static bool measure_environment( float *temperature, float *humidity )
       return( true );
     }
   }
-
   return( false );
 }
 
@@ -47,16 +46,37 @@ void loop( )
 {
   float temperature;
   float humidity;
+  // lcd.setCursor(0, 0);
+  // lcd.print(temperature);
+
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+  // lcd.print(millis() / 1000);
 
   /* Measure temperature and humidity.  If the functions returns
      true, then a measurement is available. */
   if( measure_environment( &temperature, &humidity ) == true )
   {
+    lcd.setCursor(0, 0);
+    lcd.print("Temp: ");
+
+    lcd.setCursor(6, 0);
+    lcd.print(temperature, 1);
+
+    lcd.setCursor(11, 0);
+    lcd.print("*C");
+
+    lcd.setCursor(0, 1);
+    lcd.print("Hum: ");
+
+    lcd.setCursor(6, 1);
+    lcd.print(humidity, 1);
+
+    lcd.setCursor(11, 1);
+    lcd.print("%");
+
     Serial.print( "T = " );
     Serial.print( temperature, 1 );
     Serial.print( " deg. C, H = " );
