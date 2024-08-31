f99f3ace367e2c6c03f497494c94f210c8854796
Frederik Feldmeier <74424903+elfeldos@users.noreply.github.com>
2024-08-31 15:33:38 +0200

    increased time between measurements to 5 mins

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ static bool measure_environment( float *temperature, float *humidity )
   static unsigned long measurement_timestamp = millis( );
 
   /* Measure once every four seconds. */
-  if( millis( ) - measurement_timestamp > 3000ul )
+  if( millis( ) - measurement_timestamp > 300000ul )
   {
     if( dht_sensor.measure( temperature, humidity ) == true )
     {
